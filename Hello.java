a87f49bc0d2eaed02a683b63cfd154d6b5d5f859
ankitkumar-025 <ak0199374@gmail.com>
2025-01-14 13:22:46 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+class Hello
+{
+    int y;
+    float z;
+    boolean b;
+    public static void main(String[]args)
+    {
+        Hello fd= new Hello();//To
+        
+        int x=5;
+        float f=2.5f;
+        boolean b=true;
+        System.out.println(fd.y);
+        System.out.println(fd.z);
+        System.out.println(fd.b);
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(f);
+
+
+
+
+
+        System.out.println("Hello World");
+    }
+}
