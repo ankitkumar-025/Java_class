52f606484233bf69b23df73367f7700b88ab1032
Ankit kumar gupta <ak0199374@gmail.com>
2025-02-19 23:53:06 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class prime {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter lower limit X:");
+        int x=sc.nextInt();
+        System.out.println("Enter upper limit Y:");
+        int y=sc.nextInt();
+        System.out.println("Enter Number of Primes(N):");
+        int n=sc.nextInt();
+
+        int count=0;
+        System.out.println(("Prime numbers between numbers are:"));
+
+        for(int i=x;i<=y;i++){
+            if(i<2)continue;
+
+            boolean isPrime=true;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime){
+                System.out.println(i + " ");
+                count++;
+                if(count==n)break;
+            }
+        }
+            if(count<n){
+                System.out.println("fewer number in range");
+            }
+        
+            sc.close();
+        }
+    }
