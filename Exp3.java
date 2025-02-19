7c8f87a23978b739f651bbd21f0d06441324e0b8
Ankit kumar gupta <ak0199374@gmail.com>
2025-02-19 23:52:26 +0530

    Add files via upload

---
+++ Exp3.java
@@ -0,0 +1,10 @@
+public class Exp3 {
+    public static void main(String[]args){
+        System.out.println("ACII values of numbers (0-9)");
+        for(int i=0;i<=9;i++){
+        
+        }
+
+    }
+    
+}
